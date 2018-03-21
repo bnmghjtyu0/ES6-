6f2f3fc1edcd035a52df3ce771a97985f4fc0f0a
bnmghjtyu0 <bnmghjtyu0@gmail.com>
2018-03-21 17:25:59 +0800

    更新

--- 06-2 二維陣列/02.過濾相同陣列.js
+++ 06-2 二維陣列/02.過濾相同陣列.js
@@ -1,22 +1,14 @@
-// 二維陣列相等
-
-let Array = [[0, 0], [0, 1], [1, 0], [1, 1], [0, 0], [-1, 0], [-1, 0]];
-let str = "";
-let resultArray = [];
+// 題目: 刪除 array 相同的數值
+let array = [[0, 0], [0, 1], [1, 0], [1, 1], [0, 0], [-1, 0], [-1, 0]];
 
 // 第一步: 陣列轉字串 forEach
-Array.forEach((current_value, index, array) => {
-  str = current_value.toString();
-  resultArray.push(str);
-});
-// 第二步: 重複數據刪除 array.filter
-let deduped = resultArray.filter((current_value, index, array) => {
-  return array.indexOf(current_value) == index;
-});
-// ["0,0", "0,1", "1,0", "1,1", "-1,0"]
+let arrayTostring = obj => Object.keys(obj).map(k => obj[k].toString());
+const resultArray = arrayTostring(array)
+
+// 第二步: 重複數據刪除 [...new Set(array)]
+var deduped = [...new Set(resultArray)];
 
 // 第三步: 二維字串轉陣列
-
 let string2array = deduped.map( o => {
   let [x, y] = o.split(",");
   return [+x, +y];
